e9e1ea1a935406aa792c7d5a1e57f670cacc09b2
Petrik Diak <diak@petrik.hu>
2026-01-21 09:23:13 +0100

    hatter beallitva, hit box optimalizalva. a jatek gerince megvan. loop ok, pontok illetve mas aprosagok interglasa és optimalizalasara van szukseg. credit mohamed

--- FlappyBirdGame/MainWindow.xaml.cs
+++ FlappyBirdGame/MainWindow.xaml.cs
@@ -3,15 +3,20 @@ using System.Windows;
 using System.Windows.Input;
 using System.Windows.Threading;
 using System.Windows.Controls;
+using WpfAnimatedGif;
+using System;
+using System.Windows.Media.Imaging;
 
 namespace FlappyBirdGame
 {
 	public partial class MainWindow : Window
 	{
 		DispatcherTimer gameTimer = new DispatcherTimer();
+
 		double gravity = 1.5;
 		double birdSpeed = 0;
-		double gameSpeed = 5; // gyorsabb játék
+		double gameSpeed = 5;
+
 		int score = 0;
 		bool gameOver = false;
 
@@ -26,25 +31,37 @@ namespace FlappyBirdGame
 		{
 			MyCanvas.Focus();
 			gameTimer.Start();
+
+
+			// GIF betöltése háttérként
+			var image = new BitmapImage(new Uri("Images/hatter.gif", UriKind.Relative));
+			ImageBehavior.SetAnimatedSource(backgroundGif, image);
 		}
 
 		private void GameLoop(object sender, EventArgs e)
 		{
 			if (gameOver) return;
 
-			// Madár mozgás
+
 			Canvas.SetTop(flappyBird, Canvas.GetTop(flappyBird) + birdSpeed);
 			birdSpeed += gravity;
 
-			// Csövek mozgatása és pontozás
+
 			MovePipe(pipeTop1, pipeBottom1);
 			MovePipe(pipeTop2, pipeBottom2);
 			MovePipe(pipeTop3, pipeBottom3);
 
-			// Ütközés ellenőrzés
+
 			if (CheckCollision(pipeTop1) || CheckCollision(pipeBottom1) ||
 				CheckCollision(pipeTop2) || CheckCollision(pipeBottom2) ||
 				CheckCollision(pipeTop3) || CheckCollision(pipeBottom3))
+			{
+				EndGame();
+			}
+
+
+			if (Canvas.GetTop(flappyBird) < 0 ||
+				Canvas.GetTop(flappyBird) + flappyBird.Height > MyCanvas.ActualHeight)
 			{
 				EndGame();
 			}
@@ -53,31 +70,49 @@ namespace FlappyBirdGame
 		private void MovePipe(Image top, Image bottom)
 		{
 			double left = Canvas.GetLeft(top) - gameSpeed;
+
 			Canvas.SetLeft(top, left);
 			Canvas.SetLeft(bottom, left);
 
-			// Pontszám növelése, ha a madár elhalad a cső mellett
-			if (!gameOver && left + top.Width < Canvas.GetLeft(flappyBird) && top.Tag?.ToString() != "passed")
+
+			if (!gameOver &&
+				left + top.Width < Canvas.GetLeft(flappyBird) &&
+				top.Tag?.ToString() != "passed")
 			{
 				score++;
 				scoreText.Content = "Score: " + score;
 				top.Tag = "passed";
 			}
 
-			// Ha a cső kiment a képernyőről, visszaállítjuk
+
 			if (left < -top.Width)
 			{
 				Canvas.SetLeft(top, 800);
 				Canvas.SetLeft(bottom, 800);
-
-				top.Tag = null; // újra pontozható
+				top.Tag = null;
 			}
 		}
 
 		private bool CheckCollision(Image pipe)
 		{
-			Rect birdRect = new Rect(Canvas.GetLeft(flappyBird), Canvas.GetTop(flappyBird), flappyBird.Width, flappyBird.Height);
-			Rect pipeRect = new Rect(Canvas.GetLeft(pipe), Canvas.GetTop(pipe), pipe.Width, pipe.Height);
+
+			Rect birdRect = new Rect(
+				Canvas.GetLeft(flappyBird) + 8,
+				Canvas.GetTop(flappyBird) + 8,
+				flappyBird.Width - 16,
+				flappyBird.Height - 16
+			);
+
+
+			double pipePaddingX = pipe.Width * 0.35;
+			double pipePaddingY = 0;
+
+			Rect pipeRect = new Rect(
+				Canvas.GetLeft(pipe) + pipePaddingX,
+				Canvas.GetTop(pipe) + pipePaddingY,
+				pipe.Width - (pipePaddingX * 2),
+				pipe.Height
+			);
 
 			return birdRect.IntersectsWith(pipeRect);
 		}
@@ -85,6 +120,7 @@ namespace FlappyBirdGame
 		private void EndGame()
 		{
 			if (gameOver) return;
+
 			gameOver = true;
 			gameTimer.Stop();
 			restartButton.Visibility = Visibility.Visible;
@@ -93,35 +129,46 @@ namespace FlappyBirdGame
 
 		private void RestartButton_Click(object sender, RoutedEventArgs e)
 		{
-			// Játék újraindítása
+
 			Canvas.SetTop(flappyBird, 200);
 			Canvas.SetLeft(flappyBird, 50);
 			birdSpeed = 0;
 
+
 			Canvas.SetLeft(pipeTop1, 400);
 			Canvas.SetLeft(pipeBottom1, 400);
+
 			Canvas.SetLeft(pipeTop2, 650);
 			Canvas.SetLeft(pipeBottom2, 650);
+
 			Canvas.SetLeft(pipeTop3, 900);
 			Canvas.SetLeft(pipeBottom3, 900);
 
 			score = 0;
 			scoreText.Content = "Score: 0";
 
-			gameOver = false;
-			restartButton.Visibility = Visibility.Hidden;
-
 			pipeTop1.Tag = null;
 			pipeTop2.Tag = null;
 			pipeTop3.Tag = null;
 
+			gameOver = false;
+			restartButton.Visibility = Visibility.Hidden;
 			gameTimer.Start();
 		}
 
 		private void KeyIsDown(object sender, KeyEventArgs e)
 		{
 			if (e.Key == Key.Space && !gameOver)
+			{
 				birdSpeed = -7;
+			}
 		}
+
+
+
+
+
+
+
 	}
 }
