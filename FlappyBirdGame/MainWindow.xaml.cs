f0ad5dd19362faf339c6ed0d3471d797e9e791dc
Petrik Diak <diak@petrik.hu>
2026-01-21 10:29:40 +0100

    eső, köd gyorsaság, háttér high score, score optimalizálva

--- FlappyBirdGame/MainWindow.xaml.cs
+++ FlappyBirdGame/MainWindow.xaml.cs
@@ -3,22 +3,30 @@ using System.Windows;
 using System.Windows.Input;
 using System.Windows.Threading;
 using System.Windows.Controls;
-using WpfAnimatedGif;
-using System;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Collections.Generic;
 using System.Windows.Media.Imaging;
+using WpfAnimatedGif;
 
 namespace FlappyBirdGame
 {
 	public partial class MainWindow : Window
 	{
 		DispatcherTimer gameTimer = new DispatcherTimer();
+		Random rnd = new Random();
 
 		double gravity = 1.5;
 		double birdSpeed = 0;
 		double gameSpeed = 5;
+		double birdAngle = 0;
 
 		int score = 0;
+		int highScore = 0;
 		bool gameOver = false;
+
+		List<Line> rainDrops = new List<Line>();
+		Rectangle fogOverlay = new Rectangle();
 
 		public MainWindow()
 		{
@@ -32,25 +40,36 @@ namespace FlappyBirdGame
 			MyCanvas.Focus();
 			gameTimer.Start();
 
+			var bgImage = new BitmapImage(new Uri("Images/hatter.gif", UriKind.Relative));
+			ImageBehavior.SetAnimatedSource(backgroundGif, bgImage);
 
-			// GIF betöltése háttérként
-			var image = new BitmapImage(new Uri("Images/hatter.gif", UriKind.Relative));
-			ImageBehavior.SetAnimatedSource(backgroundGif, image);
+			fogOverlay.Width = MyCanvas.ActualWidth;
+			fogOverlay.Height = MyCanvas.ActualHeight;
+			fogOverlay.Fill = new SolidColorBrush(Color.FromArgb(100, 200, 200, 200));
+			fogOverlay.Visibility = Visibility.Hidden;
+			Canvas.SetLeft(fogOverlay, 0);
+			Canvas.SetTop(fogOverlay, 0);
+			Panel.SetZIndex(fogOverlay, 15);
+			MyCanvas.Children.Add(fogOverlay);
+
+			UpdateHighScoreText();
 		}
 
 		private void GameLoop(object sender, EventArgs e)
 		{
 			if (gameOver) return;
 
-
 			Canvas.SetTop(flappyBird, Canvas.GetTop(flappyBird) + birdSpeed);
 			birdSpeed += gravity;
 
+			birdAngle += (birdSpeed > 0 ? 1 : -1);
+			if (birdAngle > 25) birdAngle = 25;
+			if (birdAngle < -25) birdAngle = -25;
+			flappyBird.RenderTransform = new RotateTransform(birdAngle, flappyBird.Width / 2, flappyBird.Height / 2);
 
 			MovePipe(pipeTop1, pipeBottom1);
 			MovePipe(pipeTop2, pipeBottom2);
 			MovePipe(pipeTop3, pipeBottom3);
-
 
 			if (CheckCollision(pipeTop1) || CheckCollision(pipeBottom1) ||
 				CheckCollision(pipeTop2) || CheckCollision(pipeBottom2) ||
@@ -59,12 +78,24 @@ namespace FlappyBirdGame
 				EndGame();
 			}
 
-
 			if (Canvas.GetTop(flappyBird) < 0 ||
 				Canvas.GetTop(flappyBird) + flappyBird.Height > MyCanvas.ActualHeight)
 			{
 				EndGame();
 			}
+
+			if (score % 5 == 0 && score != 0) gameSpeed = 5 + score / 5;
+
+			if (score >= 20)
+			{
+				StartRain();
+			}
+			if (score >= 30)
+			{
+				fogOverlay.Visibility = Visibility.Visible;
+			}
+
+			AnimateRain();
 		}
 
 		private void MovePipe(Image top, Image bottom)
@@ -74,7 +105,6 @@ namespace FlappyBirdGame
 			Canvas.SetLeft(top, left);
 			Canvas.SetLeft(bottom, left);
 
-
 			if (!gameOver &&
 				left + top.Width < Canvas.GetLeft(flappyBird) &&
 				top.Tag?.ToString() != "passed")
@@ -82,8 +112,8 @@ namespace FlappyBirdGame
 				score++;
 				scoreText.Content = "Score: " + score;
 				top.Tag = "passed";
+				UpdateHighScoreText();
 			}
-
 
 			if (left < -top.Width)
 			{
@@ -95,14 +125,12 @@ namespace FlappyBirdGame
 
 		private bool CheckCollision(Image pipe)
 		{
-
 			Rect birdRect = new Rect(
 				Canvas.GetLeft(flappyBird) + 8,
 				Canvas.GetTop(flappyBird) + 8,
 				flappyBird.Width - 16,
 				flappyBird.Height - 16
 			);
-
 
 			double pipePaddingX = pipe.Width * 0.35;
 			double pipePaddingY = 0;
@@ -124,23 +152,23 @@ namespace FlappyBirdGame
 			gameOver = true;
 			gameTimer.Stop();
 			restartButton.Visibility = Visibility.Visible;
-			MessageBox.Show("GAME OVER!");
+
+			if (score > highScore) highScore = score;
+			UpdateHighScoreText();
 		}
 
 		private void RestartButton_Click(object sender, RoutedEventArgs e)
 		{
-
 			Canvas.SetTop(flappyBird, 200);
 			Canvas.SetLeft(flappyBird, 50);
 			birdSpeed = 0;
-
+			birdAngle = 0;
+			flappyBird.RenderTransform = new RotateTransform(0, flappyBird.Width / 2, flappyBird.Height / 2);
 
 			Canvas.SetLeft(pipeTop1, 400);
 			Canvas.SetLeft(pipeBottom1, 400);
-
 			Canvas.SetLeft(pipeTop2, 650);
 			Canvas.SetLeft(pipeBottom2, 650);
-
 			Canvas.SetLeft(pipeTop3, 900);
 			Canvas.SetLeft(pipeBottom3, 900);
 
@@ -151,8 +179,29 @@ namespace FlappyBirdGame
 			pipeTop2.Tag = null;
 			pipeTop3.Tag = null;
 
+			gameSpeed = 5;
+			rainDrops.Clear();
+			fogOverlay.Visibility = Visibility.Hidden;
+
 			gameOver = false;
 			restartButton.Visibility = Visibility.Hidden;
+
+			// Csak a dinamikus elemeket töröljük (pl. eső)
+			List<UIElement> toRemove = new List<UIElement>();
+			foreach (UIElement child in MyCanvas.Children)
+			{
+				if (child != backgroundGif && child != flappyBird && child != pipeTop1 && child != pipeBottom1 &&
+					child != pipeTop2 && child != pipeBottom2 && child != pipeTop3 && child != pipeBottom3 &&
+					child != scoreText && child != highScoreText && child != restartButton && child != fogOverlay)
+				{
+					toRemove.Add(child);
+				}
+			}
+			foreach (var child in toRemove)
+			{
+				MyCanvas.Children.Remove(child);
+			}
+
 			gameTimer.Start();
 		}
 
@@ -161,14 +210,48 @@ namespace FlappyBirdGame
 			if (e.Key == Key.Space && !gameOver)
 			{
 				birdSpeed = -7;
+				birdAngle = -20;
 			}
 		}
 
+		private void StartRain()
+		{
+			if (rainDrops.Count > 0) return;
+			for (int i = 0; i < 100; i++)
+			{
+				Line drop = new Line();
+				drop.Stroke = Brushes.LightBlue;
+				drop.StrokeThickness = 2;
+				drop.X1 = rnd.NextDouble() * MyCanvas.ActualWidth;
+				drop.Y1 = rnd.NextDouble() * MyCanvas.ActualHeight;
+				drop.X2 = drop.X1;
+				drop.Y2 = drop.Y1 + 10;
+				Panel.SetZIndex(drop, 12);
+				MyCanvas.Children.Add(drop);
+				rainDrops.Add(drop);
+			}
+		}
 
+		private void AnimateRain()
+		{
+			if (rainDrops.Count == 0) return;
+			foreach (var drop in rainDrops)
+			{
+				drop.Y1 += 5;
+				drop.Y2 += 5;
+				if (drop.Y1 > MyCanvas.ActualHeight)
+				{
+					drop.Y1 = 0;
+					drop.Y2 = 10;
+					drop.X1 = rnd.NextDouble() * MyCanvas.ActualWidth;
+					drop.X2 = drop.X1;
+				}
+			}
+		}
 
-
-
-
-
+		private void UpdateHighScoreText()
+		{
+			highScoreText.Content = "High Score: " + highScore;
+		}
 	}
 }
